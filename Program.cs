beacfd7f6153ef120ee08064020cb95988f4299c
DaniilStropolov <daniil.stropolov@rambler.ru>
2025-01-30 00:24:28 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string myName;
+        myName = "Даниил";
+        Console.WriteLine(myName);
+        Console.ReadKey();
+    }
+}
