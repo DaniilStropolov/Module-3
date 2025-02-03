0726dfccfa8e0b8cd2d90a49ca71ead1e5d3bf12
DaniilStropolov <daniil.stropolov@rambler.ru>
2025-02-04 00:27:46 +0300

    Создал программу "Анкета" как отдельный проект

---
+++ Questionnaire/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        string Name = "Daniel";
+        int Age = 21;
+        bool Pet = false;
+        double ShoeSize = 41.5;
+        Console.WriteLine("My name is " + Name);
+        Console.WriteLine("My age is " + Age);
+        Console.WriteLine("Do I have a pet? " + Pet);
+        Console.WriteLine("My shoe size is " + ShoeSize);
+        Console.ReadKey();
+    }
+}
