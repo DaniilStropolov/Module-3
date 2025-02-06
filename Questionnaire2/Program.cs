7796751e5fa021503afd93b6b4b5f4cecc40b415
DaniilStropolov <daniil.stropolov@rambler.ru>
2025-02-07 00:31:51 +0300

    Создал вторую программу для анкетирования

---
+++ Questionnaire2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Questionnaire2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свое имя: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Введите свой возраст: ");
+            byte age = Convert.ToByte(Console.ReadLine());
+
+            Console.WriteLine($"Тебя зовут {name} и тебе {age}");
+
+            Console.Write("Введите дату своего рождения: ");
+            string birthDate = Console.ReadLine();
+            Console.WriteLine($"Вы родились {birthDate}");
+        }
+    }
+}
